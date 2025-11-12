217ae2afbd8644048250d5830bb54198f4ca6960
Santiago <santiago800g@gmail.com>
2025-11-12 12:14:45 -0600

    Add the logic of the human choice

--- code.js
+++ code.js
@@ -9,3 +9,21 @@ function getComputerChoice() {
             return 'scissors'
     }
 }
+
+function getHumanChoice() {
+    let choice = prompt('Make your choice: rock, paper, scissors? ');
+    if(choice === null) {
+        return getHumanChoice();
+    }
+    choice = choice.toLowerCase();
+    switch(choice) {
+        case 'rock':
+            return choice;
+        case 'paper':
+            return choice;
+        case 'scissors':
+            return choice;
+        default:
+            return getHumanChoice();    
+    }
+}
